54f52723856f2b4fab5b620e3275a1a6b388ae01
AGLOP-1354 <jake@jober.io>
2025-02-05 22:20:48 +0900

    [게시글] 생성 dto 정의

---
+++ src/post/dto/create-post.dto.ts
@@ -0,0 +1,25 @@
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+} from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsUrl()
+  @IsOptional()
+  imageUrl?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isAnonymous?: boolean;
+}
