228990d7dd2a3615f5b752597d8e527cdcef24fe
AGLOP-1354 <jake@jober.io>
2025-03-19 13:37:59 +0900

    [댓글] 좋아요 엔티티 정의

---
+++ src/comment/entities/comment-like.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, JoinColumn, ManyToOne } from 'typeorm';
+
+import { BaseEntity } from 'src/common/entity/base-entity.entity';
+import { UserEntity } from 'src/user/entities/user.entity';
+
+import { CommentEntity } from './comment.entity';
+
+@Entity('comment_like')
+export class CommentLikeEntity extends BaseEntity {
+  @ManyToOne(() => CommentEntity, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'comment_id' })
+  comment: CommentEntity;
+
+  @ManyToOne(() => UserEntity, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user: UserEntity;
+}
